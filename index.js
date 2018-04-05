ebe4ce537d4d4a4b0ebc8698bcdbb45d3f6e1a11
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 02:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ function createPost() {
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-
   let postTitle = document.getElementById("postTitle").value;
   let postAuthor = document.getElementById("postAuthor").value;
   let postContent = document.getElementById("postContent").value;
 
-  var postTemplate = document.getElementById("post-template").innerHTML;
-  var templateFn = _.template(postTemplate);
+  let postTemplate = document.getElementById("post-template").innerHTML;
+  let templateFn = _.template(postTemplate);
 
-  var postDiv = document.getElementById("post");
-  var templateHTML = templateFn({ 'post': postContent, 'poster': postAuthor, 'title': postTitle});
+  let postDiv = document.getElementById("post");
+  let templateHTML = templateFn({ 'post': postContent, 'poster': postAuthor, 'title': postTitle});
 
   postDiv.innerHTML += templateHTML;
 }
 
 function postComment() {
-  var commenter = document.getElementById("commenterName").value;
-  var comment = document.getElementById("commentText").value;
-  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  let commenter = document.getElementById("commenterName").value;
+  let comment = document.getElementById("commentText").value;
+  let commentTemplate = document.getElementById("comment-template").innerHTML;
 
-  var templateFn = _.template(commentTemplate);
+  let templateFn = _.template(commentTemplate);
 }
