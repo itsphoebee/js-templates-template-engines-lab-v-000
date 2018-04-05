80a172350c1cc1e739f09ecd07b19af4c9e4028b
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 03:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function createPost() {
   let postDiv = document.getElementById("post");
   let templateHTML = postTemplate({ 'post': postContent, 'poster': postAuthor, 'title': postTitle});
 
-  postDiv += templateHTML;
+  postDiv.innerHTML += templateHTML;
 }
 
 function postComment() {
@@ -22,5 +22,5 @@ function postComment() {
   let templateHTML = commentTemplate({ 'commenter': commenter, 'comment': comment});
   let commentsTemplate = document.getElementById("comments");
 
-  commentsTemplate += templateHTML;
+  commentsTemplate.innerHTML += templateHTML;
 }
