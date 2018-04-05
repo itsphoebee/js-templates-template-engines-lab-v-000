56e750a513f35eaa061c819704696015a701f6eb
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 00:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function postComment() {
-  var commenter = document.getElementById("commenterName").value;
-  var comment = document.getElementById("commentText").value;
-  var commentTemplate = document.getElementById("comment-template").innerHTML;
-  var templateFn = _.template(commentTemplate);
-  var commentsDiv = document.getElementById("comments");
-  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+function createPost() {
+  var poster = document.getElementById("posterName").value;
+  var post = document.getElementById("postContent").value;
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  var templateFn = _.template(postTemplate);
+  var postsDiv = document.getElementById("posts");
+  var templateHTML = templateFn({ 'post': post, 'poster': poster });
  
-  commentsDiv.innerHTML += templateHTML;
+  postsDiv.innerHTML += templateHTML;
 }
