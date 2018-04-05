ec213c6619c7e707ca6378a12db4ffa20b916e31
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 01:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function createPost() {
 
   postsDiv.innerHTML += templateHTML;
 }
+
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+
+  var templateFn = _.template(commentTemplate);
+}
