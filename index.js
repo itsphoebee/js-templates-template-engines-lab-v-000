92323b76215de2f0aca24355ded0ba44d88c359a
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 03:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function createPost() {
 
 function postComment() {
   let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
   let commenter = document.getElementById("commenterName").value;
   let comment = document.getElementById("commentText").value;
 
