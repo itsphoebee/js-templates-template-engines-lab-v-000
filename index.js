3b1053380c89c4bd13dd1a4966d246e4a25991dd
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 02:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost() {
-  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);  
-  let commentsTemplate = document.getElementById("comments-template").innerHTML;
-  let postTemplate = document.getElementById("post-template").innerHTML;
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
 
   let postTitle = document.getElementById("postTitle").value;
   let postAuthor = document.getElementById("postAuthor").value;
