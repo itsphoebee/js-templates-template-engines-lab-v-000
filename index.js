ef22aa95b76ffd79ce7ec100b67d05568f9be6ae
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 02:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function createPost() {
   var templateFn = _.template(postTemplate);
 
   var postsDiv = document.getElementById("posts");
-  var templateHTML = templateFn({ 'post': postContent, 'poster': poster });
+  var templateHTML = templateFn({ 'post': postContent, 'poster': postAuthor, 'title': postTitle});
 
   postsDiv.innerHTML += templateHTML;
 }
