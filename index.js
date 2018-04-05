360dbfa1acf311c84a1bab570a7f6e9c8d479680
Phoebe Lau <phoebelau7@gmail.com>
2018-04-05 04:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function createPost() {
+
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
 
   let postTitle = document.getElementById("postTitle").value;
   let postAuthor = document.getElementById("postAuthor").value;
-  let postContent = document.getElementById("postBody").value;
+  let post = document.getElementById("postBody").value;
 
-  document.getElementsByTagName('main')[0].innerHTML += pageTemplate();
 
-  let templateHTML = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor })
   let commentsSection = commentsTemplate();
-  let postDiv = document.getElementById('post')
+  let postElement = document.getElementById('post');
 
-  postDiv.innerHTML += templateHTML;
-  postDiv.getElementsByTagName("footer")[0] += commentsSection;
-  }
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
 
   function postComment() {
     let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
